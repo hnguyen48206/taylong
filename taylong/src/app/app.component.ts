abc9b23a46d100479c7530a2c9835cb0af52c7c2
hoang nguyen <hnguyen48206@gmail.com>
2022-05-02 15:07:14 +0700

    abc

--- taylong/src/app/app.component.ts
+++ taylong/src/app/app.component.ts
@@ -7,32 +7,31 @@ import { HomePage } from '../pages/home/home';
 import { GlobalHeroProvider } from '../providers/global-hero/global-hero';
 import { Storage } from '@ionic/storage';
 
-declare var cordova:any
+declare var cordova: any
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage:any = HomePage;
+  rootPage: any = HomePage;
 
-  constructor(storage:Storage, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, hero: GlobalHeroProvider) {
+  constructor(storage: Storage, platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen, hero: GlobalHeroProvider) {
     platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       statusBar.styleDefault();
       splashScreen.hide();
-  
-      if(platform.is('cordova'))
-      {
+
+      if (platform.is('cordova')) {
         cordova.KioskPlugin.setKioskEnabled(true)
-        cordova.KioskPlugin.setAllowedKeys([ 24, 25, 26 ]);
-        cordova.KioskPlugin.setCloseSystemDialogIntervalMillis(200)
-        cordova.KioskPlugin.setCloseSystemDialogDurationMillis(20000)
+        cordova.KioskPlugin.setAllowedKeys([24, 25, 26]);
+        cordova.KioskPlugin.setCloseSystemDialogIntervalMillis(200);
+        cordova.KioskPlugin.setCloseSystemDialogDurationMillis(20000);
       }
-      
-      hero.networkStatus =  navigator.onLine ? "Online" : "OFFline";
+
+      hero.networkStatus = navigator.onLine ? "Online" : "OFFline";
       console.log(hero.networkStatus)
-      
+
     });
   }
 }
